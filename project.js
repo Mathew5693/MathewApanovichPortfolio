1756df4c1b298c638af1c7c933440d682fbe3fb2
Mathew Apanovich <Mapanovich@yahoo.com>
2019-08-02 14:36:55 -0400

    First Commit

---
+++ project.js
@@ -0,0 +1,32 @@
+const STORE = [
+  {
+  project:'Interactive Web App',
+  desc:'A Web App Movie Quiz using Forms',
+  skills:'CSS, HTML5, JS',
+  repo:'https://github.com/Mathew5693/QuizAppWireFrame',
+  app:'https://quizapp--mathew5693.repl.co/',
+  thumb: 'proj1thumb.jpg'
+  },
+  {
+  project:'Test 2',
+  desc:'This is a description of proj 2',
+  skills:'skills/logos',
+  repo:'https://github.com/Mathew5693',
+  app:'https://repl.it/@Mathew5693/QuizApp'
+  }
+  ,
+  {
+  project:'Test 3',
+  desc:'this is a description of proj 3',
+  skills:'skills/logos',
+  repo:'https://github.com/Mathew5693',
+  app:'https://repl.it/@Mathew5693/QuizApp'
+  },
+  {
+  project:'Test 4',
+  desc:'this is a description of proj 4',
+  skills:'skills/logos',
+  repo:'https://github.com/Mathew5693',
+  app:'https://repl.it/@Mathew5693/QuizApp'
+  }
+];
---
+++ script.js
@@ -0,0 +1,88 @@
+function throwProject(){
+  $('.catchProject').each(generateProject => {
+
+    $('.displayProject').html(
+      `
+      <div class="row">
+        <div class="column">
+        <section>
+            <div class="project p1">
+            
+              <ul>
+        <li>${STORE[0].project}</li>
+        <li><a href="${STORE[0].thumb}" target="_blank"><img src="${STORE[0].thumb}"></a></li>
+        <li><a href="${STORE[0].app}" target="_blank">App Demo</a></li>
+        <li><a href="${STORE[0].repo}" target="_blank">Repository</a></li>
+        <li>${STORE[0].skills}</li>
+        <li class="desc">${STORE[0].desc}</li>
+              </ul>
+            </div>
+        </section>
+        </div>
+
+        <div class="column">
+        <section>
+            <div class="project p2">
+            
+           </div>
+        </section>
+        </div>
+      </div>
+      <div class="row">
+        <div class="column">
+        <section>
+            <div class="project p3">
+
+            </div>
+        </section>
+        </div>
+      <div class="column">
+        <section>
+            <div class="project p4">
+            
+           </div>
+        </section>
+        </div>
+      </div>
+      `
+      );
+  });
+  
+}
+
+function showDesc(){
+
+$('.project.p1').mouseenter(function(event) {
+
+	$('.project.p1 ul li.desc').show();
+});
+$('.project.p2').mouseenter(function(event) {
+
+	$('.project.p2 ul li.desc').show();
+});
+
+$('.project.p3').mouseenter(function(event) {
+
+	$('.project.p3 ul li.desc').show();
+});
+
+$('.project.p4').mouseenter(function(event) {
+
+	$('.project.p4 ul li.desc').show();
+});
+
+$('.project').mouseleave(function(event) {
+
+	$('.project li.desc').hide();
+});
+
+}
+
+function sendProject(){
+  throwProject();
+  showDesc();
+  //more to come
+  
+}
+
+$(sendProject);
